2aa9846e9b903fe5a1826cfc3af6cc7cd901b039
Bibosiandre <vlaaad02@mail.ru>
2023-02-20 10:52:50 +0300

    Добавьте файлы проекта.

---
+++ TestConsoleApp/AdditionalFiles/TestClass2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp.AdditionalFiles
+{
+    public class TestClass2
+    {
+        //Test comment
+
+        /// <summary>
+        /// Отображает переданную информацию в строку
+        /// </summary>
+        /// <param name="Name">Имя</param>
+        /// <param name="Surname">Фамилия</param>
+        /// <remarks>Дополнительная инфа какая-то</remarks>
+        public void DisplayName(string Name, string Surname)
+        {
+            Console.WriteLine($"Привет! Меня зовут {Name}, фамилия {Surname}");
+        }
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using TestConsoleApp.AdditionalFiles;
+
+namespace TestConsoleApp;
+
+
+public class MainClass
+{
+    static void Main()
+    {
+        Execution execution= new Execution();
+        execution.Execute();
+    }
+}
+
+public class Execution
+{
+    TestClass2 test = new TestClass2();
+    public void Execute()
+    {
+        Console.WriteLine("Hello world!");
+        test.DisplayName("Мужик", "Таджик");
+    }
+}
